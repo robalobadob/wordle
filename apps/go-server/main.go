bf1dfa1cd5d76ba2fc4b4dbd98a53aea65ff9468
Rob Hoover <rtrhoover@gmail.com>
2025-08-24 11:15:55 -0700

    hooks in words package to main

--- apps/go-server/main.go
+++ apps/go-server/main.go
@@ -4,20 +4,25 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"os"
 
 	"github.com/robalobadob/wordle/apps/go-server/internal/httpserver"
 	"github.com/robalobadob/wordle/apps/go-server/internal/store"
+	"github.com/robalobadob/wordle/apps/go-server/internal/words"
+	"os"
 )
 
 func main() {
 	_ = godotenv.Load()
-	level, err := zerolog.ParseLevel(getEnv("LOG_LEVEL", "info"))
-	if err == nil { zerolog.SetGlobalLevel(level) }
+	if lvl, err := zerolog.ParseLevel(getEnv("LOG_LEVEL", "info")); err == nil {
+		zerolog.SetGlobalLevel(lvl)
+	}
+
+	if err := words.Init(); err != nil {
+		log.Fatal().Err(err).Msg("failed to load word lists")
+	}
 
 	mem := store.NewMemoryStore()
 	srv := httpserver.New(mem)
-
 	port := getEnv("PORT", "5175")
 	log.Info().Str("port", port).Msg("starting go-server")
 	if err := srv.Start(":" + port); err != nil {
