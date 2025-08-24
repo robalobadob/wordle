14fd6795dfe215f47ed56204819c5aece38077d0
Rob Hoover <rtrhoover@gmail.com>
2025-08-24 10:32:33 -0700

    adds function to handle CORS

--- apps/go-server/internal/httpserver/server.go
+++ apps/go-server/internal/httpserver/server.go
@@ -45,6 +45,20 @@ func jsonMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+s.r.Use(func(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusNoContent)
+            return
+        }
+        next.ServeHTTP(w, r)
+    })
+})
+
+
 // --- Handlers ---
 
 type newGameReq struct {
